d5bbdaaba9719fe3a39711941ab4056260baed8c
Sergey <you@example.com>
2025-04-11 21:11:01 +0300

    add hello_world endpoint

--- films-catalog/main.py
+++ films-catalog/main.py
@@ -1,5 +1,21 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import (
+    FastAPI,
+    Request,
+)
 
 app = FastAPI(
     title="Films App",
 )
+
+
+@app.get("/")
+async def hello_world(request: Request):
+    docs_url = request.url.replace(path="/docs", query="")
+    return {
+        "docs": str(docs_url),
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
