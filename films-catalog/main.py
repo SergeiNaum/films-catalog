9f02826b1d5eb08eff3827672eecd963cb923273
Sergey <you@example.com>
2025-04-13 19:42:46 +0300

    add andpoints get_films and get_film_detail

--- films-catalog/main.py
+++ films-catalog/main.py
@@ -4,6 +4,9 @@ from fastapi import (
     Request,
 )
 
+from constants import FILMS
+from schemas.film import FilmSchema
+
 app = FastAPI(
     title="Films App",
 )
@@ -15,6 +18,22 @@ async def hello_world(request: Request):
     return {
         "docs": str(docs_url),
     }
+
+
+@app.get("/films/")
+async def get_films() -> list[FilmSchema]:
+    return FILMS
+
+
+def get_film_by_id(movie_id: int, films: list[FilmSchema]) -> FilmSchema:
+    return next((film for film in films if movie_id == film.id), None)
+
+
+@app.get("/films/{movie_id}")
+async def get_film_detail(
+    movie_id: int,
+) -> FilmSchema | None:
+    return get_film_by_id(movie_id, FILMS)
 
 
 if __name__ == "__main__":
