8ba9e284456dd80d24a56ba0d4bbbaff7647b89d
Sergey <you@example.com>
2025-04-16 20:57:57 +0300

    use storage method get_by_slug

--- films-catalog/api/v1/films/dependencies/utils.py
+++ films-catalog/api/v1/films/dependencies/utils.py
@@ -1,10 +1,11 @@
 from fastapi import HTTPException, status
 
+from api.v1.films.crud import film_storage
 from schemas.film import FilmSchema
 
 
-def get_film_by_id(movie_id: int, films: list[FilmSchema]) -> FilmSchema | None:
-    film = next((film for film in films if movie_id == film.id), None)
+def get_film_by_slug(slug: str) -> FilmSchema | None:
+    film: FilmSchema | None = film_storage.get_by_slug(slug)
     if film:
         return film
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Фильм не найден")
