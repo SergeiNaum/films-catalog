b2ce469a0a6445197636658fea1c374fd7bcc5bd
Sergey <you@example.com>
2025-04-16 20:58:09 +0300

    refactor views

--- films-catalog/api/v1/films/views.py
+++ films-catalog/api/v1/films/views.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter
+import random
+from typing import Annotated
 
-from api.v1.films.dependencies.utils import get_film_by_id
-from api.v1.films.crud import FILMS
-from schemas.film import FilmSchema
+from annotated_types import Len
+from fastapi import APIRouter, status, Depends
 
+from api.v1.films.dependencies.utils import get_film_by_slug
+from api.v1.films.crud import film_storage
+from schemas.film import FilmSchema, FilmSchemaCreate
 
 router = APIRouter(
     prefix="/films",
@@ -13,11 +16,24 @@ router = APIRouter(
 
 @router.get("/all/")
 async def get_films() -> list[FilmSchema]:
-    return FILMS
+    return film_storage.get()
 
 
-@router.get("/{movie_id}")
-async def get_film_detail(
-    movie_id: int,
+@router.get("/{movie_slug}")
+async def get_film_details(
+    movie: Annotated[
+        get_film_by_slug,
+        Depends(get_film_by_slug),
+    ],
 ) -> FilmSchema | None:
-    return get_film_by_id(movie_id, FILMS)
+    return movie
+
+
+@router.post(
+    "/",
+    response_model=FilmSchema,
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_film(film_create: FilmSchemaCreate) -> FilmSchema:
+    obj = film_storage.create(film_create)
+    return obj
