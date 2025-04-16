2ab4e4df682a3e31c24c1728776a1e08b499ee5e
Sergey <you@example.com>
2025-04-16 20:59:00 +0300

    add FilmSchemaCreate

--- films-catalog/schemas/film.py
+++ films-catalog/schemas/film.py
@@ -1,14 +1,19 @@
-from decimal import Decimal
+from typing import Annotated
 
+from annotated_types import Len
 from pydantic import BaseModel
 
 
-class BaseFilmSchema(BaseModel):
-    id: int
+class FilmSchemaBase(BaseModel):
+    slug: str
     title: str
     description: str
-
-
-class FilmSchema(BaseFilmSchema):
     budget: int
     box_office: int
+
+
+class FilmSchemaCreate(FilmSchemaBase):
+    slug: Annotated[str, Len(3, 100)]
+
+
+class FilmSchema(FilmSchemaBase): ...
