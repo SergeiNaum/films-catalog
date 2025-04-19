1fcc3665dd72391a2707961b7f54bb6bce514fb1
Sergey <you@example.com>
2025-04-19 17:07:28 +0300

    add update_film (PUT) endpoint

--- films-catalog/api/v1/films/views/details_views.py
+++ films-catalog/api/v1/films/views/details_views.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
-from schemas.film import FilmSchema
+from schemas.film import FilmSchema, FilmSchemaUpdate
 
 from api.v1.films.crud import film_storage
 from api.v1.films.dependencies.utils import get_film_by_slug
@@ -34,3 +34,8 @@ async def get_film_details(film: FILM_BY_SLUG) -> FilmSchema | None:
 @router.delete("/", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_film(film: FILM_BY_SLUG) -> None:
     film_storage.delete(film)
+
+
+@router.put("/")
+async def update_film(film: FILM_BY_SLUG, film_in: FilmSchemaUpdate) -> FilmSchema:
+    return film_storage.update(film_schema=film, film_schema_in=film_in)
