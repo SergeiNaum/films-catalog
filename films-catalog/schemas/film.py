bda84753da9b41b7d3ff4854b353ba97c31f3a6f
Sergey <you@example.com>
2025-04-19 17:41:49 +0300

    add FilmSchemaPartialUpdate

--- films-catalog/schemas/film.py
+++ films-catalog/schemas/film.py
@@ -3,21 +3,45 @@ from typing import Annotated
 from annotated_types import Len, MaxLen
 from pydantic import BaseModel
 
+DescriptionStr = Annotated[str, MaxLen(200)]
+
 
 class FilmSchemaBase(BaseModel):
     title: str
-    description: Annotated[str, MaxLen(200)] = ""
+    description: DescriptionStr = ""
     budget: int
     box_office: int
 
 
 class FilmSchemaCreate(FilmSchemaBase):
+    """
+    Модель для создания фильма
+    """
+
     slug: Annotated[str, Len(3, 100)]
 
 
 class FilmSchemaUpdate(FilmSchemaBase):
-    description: Annotated[str, MaxLen(200)]
+    """
+    Модель для обновления информации о фильме
+    """
+
+    description: DescriptionStr
+
+
+class FilmSchemaPartialUpdate(FilmSchemaBase):
+    """
+    Модель для частичного обновления информации
+    о фильме
+    """
+
+    budget: int | None = None
+    box_office: int | None = None
+    title: str | None = None
+    description: DescriptionStr | None = None
 
 
 class FilmSchema(FilmSchemaBase):
+    """Модель фильма"""
+
     slug: str
