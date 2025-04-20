2da508c1d902f83a74a6984e4382f8a4844bbbc2
Sergey <you@example.com>
2025-04-20 21:22:08 +0300

    use FilmSchemaRead in views

--- films-catalog/api/v1/films/views/details_views.py
+++ films-catalog/api/v1/films/views/details_views.py
@@ -1,7 +1,12 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
-from schemas.film import FilmSchema, FilmSchemaPartialUpdate, FilmSchemaUpdate
+from schemas.film import (
+    FilmSchema,
+    FilmSchemaPartialUpdate,
+    FilmSchemaRead,
+    FilmSchemaUpdate,
+)
 
 from api.v1.films.crud import film_storage
 from api.v1.films.dependencies.utils import get_film_by_slug
@@ -26,7 +31,10 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get(
+    "/",
+    response_model=FilmSchemaRead,
+)
 async def get_film_details(film: FILM_BY_SLUG) -> FilmSchema | None:
     return film
 
@@ -36,11 +44,17 @@ async def delete_film(film: FILM_BY_SLUG) -> None:
     film_storage.delete(film)
 
 
-@router.put("/")
+@router.put(
+    "/",
+    response_model=FilmSchemaRead,
+)
 async def update_film(film: FILM_BY_SLUG, film_in: FilmSchemaUpdate) -> FilmSchema:
     return film_storage.update(film_schema=film, film_schema_in=film_in)
 
 
-@router.patch("/")
+@router.patch(
+    "/",
+    response_model=FilmSchemaRead,
+)
 async def partial_update_film(film: FILM_BY_SLUG, film_in: FilmSchemaPartialUpdate) -> FilmSchema:
     return film_storage.partial_update(film_schema=film, film_schema_in=film_in)
--- films-catalog/api/v1/films/views/list_views.py
+++ films-catalog/api/v1/films/views/list_views.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, status
-from schemas.film import FilmSchema, FilmSchemaCreate
+from schemas.film import FilmSchema, FilmSchemaCreate, FilmSchemaRead
 
 from api.v1.films.crud import film_storage
 
@@ -9,14 +9,17 @@ router = APIRouter(
 )
 
 
-@router.get("/all/")
+@router.get(
+    "/all/",
+    response_model=list[FilmSchemaRead],
+)
 async def get_films() -> list[FilmSchema]:
     return film_storage.get()
 
 
 @router.post(
     "/",
-    response_model=FilmSchema,
+    response_model=FilmSchemaRead,
     status_code=status.HTTP_201_CREATED,
 )
 async def create_film(film_create: FilmSchemaCreate) -> FilmSchema:
