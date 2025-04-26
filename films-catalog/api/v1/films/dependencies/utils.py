58582217cf0f2b74109d2f87aec8529d2493cc79
Sergey <you@example.com>
2025-04-26 16:59:36 +0300

    create DI function add_background_task

--- films-catalog/api/v1/films/dependencies/utils.py
+++ films-catalog/api/v1/films/dependencies/utils.py
@@ -1,7 +1,11 @@
-from fastapi import HTTPException, status
+import logging
+
+from fastapi import HTTPException, status, BackgroundTasks
 from schemas.film import FilmSchema
 
 from api.v1.films.crud import film_storage
+
+logger = logging.getLogger(__name__)
 
 
 async def get_film_by_slug(slug: str) -> FilmSchema | None:
@@ -9,3 +13,9 @@ async def get_film_by_slug(slug: str) -> FilmSchema | None:
     if film:
         return film
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Фильм не найден")
+
+
+async def add_background_task(background_task: BackgroundTasks):
+    yield
+    logger.info("adding background task")
+    background_task.add_task(film_storage.save_state_to_json)
