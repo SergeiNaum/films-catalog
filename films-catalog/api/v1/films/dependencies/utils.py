8835c10936fd0ad804bd11ffdf58daf80cc851f9
Sergey <you@example.com>
2025-04-26 17:18:42 +0300

    add request method check

--- films-catalog/api/v1/films/dependencies/utils.py
+++ films-catalog/api/v1/films/dependencies/utils.py
@@ -1,11 +1,18 @@
 import logging
 
-from fastapi import HTTPException, status, BackgroundTasks
+from fastapi import (
+    HTTPException,
+    status,
+    BackgroundTasks,
+    Request,
+)
 from schemas.film import FilmSchema
 
 from api.v1.films.crud import film_storage
 
 logger = logging.getLogger(__name__)
+
+UNSAFE_METHODS = frozenset({"POST", "PUT", "PATCH", "DELETE"})
 
 
 async def get_film_by_slug(slug: str) -> FilmSchema | None:
@@ -15,7 +22,11 @@ async def get_film_by_slug(slug: str) -> FilmSchema | None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Фильм не найден")
 
 
-async def add_background_task(background_task: BackgroundTasks):
+async def add_background_task(
+    request: Request,
+    background_task: BackgroundTasks,
+):
     yield
-    logger.info("adding background task")
-    background_task.add_task(film_storage.save_state_to_json)
+    if request.method in UNSAFE_METHODS:
+        logger.info("adding background task")
+        background_task.add_task(film_storage.save_state_to_json)
