d4b05c285e7d82196216676361f5e3148ecf9529
Sergey <you@example.com>
2025-04-26 14:02:07 +0300

    sync to async get_film_by_slug

--- films-catalog/api/v1/films/dependencies/utils.py
+++ films-catalog/api/v1/films/dependencies/utils.py
@@ -4,7 +4,7 @@ from schemas.film import FilmSchema
 from api.v1.films.crud import film_storage
 
 
-def get_film_by_slug(slug: str) -> FilmSchema | None:
+async def get_film_by_slug(slug: str) -> FilmSchema | None:
     film: FilmSchema | None = film_storage.get_by_slug(slug)
     if film:
         return film
