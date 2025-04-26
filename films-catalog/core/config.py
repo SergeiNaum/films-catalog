49f3c4cca943e0c25dac42df04ea41eb5f9faf7a
Sergey <you@example.com>
2025-04-26 14:00:25 +0300

    init core.config

---
+++ films-catalog/core/config.py
@@ -0,0 +1,12 @@
+import logging
+from pathlib import Path
+
+# /home/naum/PycharmProjects/films-catalog/films-catalog/core/config.py
+BASE_DIR = Path(__file__).resolve().parents[2]
+FILM_STORAGE_FILEPATH = BASE_DIR / "json_db.json"
+
+
+LOG_LEVEL = logging.INFO
+LOG_FORMAT: str = (
+    "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+)
