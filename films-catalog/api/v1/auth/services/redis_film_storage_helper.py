34728fa782b3ce0509804f8f62f421ed5921e76f
Sergey <you@example.com>
2025-06-11 14:11:38 +0300

    add redis film_storage_helper

---
+++ films-catalog/api/v1/auth/services/redis_film_storage_helper.py
@@ -0,0 +1,60 @@
+from core import config
+from redis import Redis
+from schemas.film import FilmSchema
+
+from api.v1.auth.services.film_storage_helper import BaseBookStorageHelper
+
+
+class RedisBookStorageHelper(BaseBookStorageHelper):
+    def __init__(
+        self,
+        host: str,
+        port: int,
+        db: int,
+        books_hash_name: str,
+        decode_responses: bool = True,
+    ) -> None:
+        self.redis = Redis(
+            host=host,
+            port=port,
+            db=db,
+            decode_responses=decode_responses,
+        )
+        self.books_hash_name = books_hash_name
+
+    def add_film(
+        self,
+        film_schema: FilmSchema,
+    ) -> None:
+        self.redis.hset(
+            name=self.books_hash_name,
+            key=film_schema.slug,
+            value=film_schema.model_dump_json(),
+        )
+
+    def delete_film(
+        self,
+        film_schema: FilmSchema,
+    ) -> None:
+        self.redis.hdel(self.books_hash_name, film_schema.slug)
+
+    def get_all_films(self) -> list:
+        result = self.redis.hvals(self.books_hash_name)
+        return result
+
+    def get_film_details(
+        self,
+        slug: str,
+    ) -> str | None:
+        return self.redis.hget(
+            name=self.books_hash_name,
+            key=slug,
+        )
+
+
+redis_films_storage = RedisBookStorageHelper(
+    host=config.REDIS_HOST,
+    port=config.REDIS_PORT,
+    db=config.REDIS_DB_FILMS,
+    books_hash_name=config.REDIS_FILMS_HASH_NAME,
+)
