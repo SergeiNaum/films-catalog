811828428069e407f531478e0d282fa21cd64230
Sergey <you@example.com>
2025-06-11 14:14:39 +0300

    remove film_storage.init_storage_from_state from lifespan

--- films-catalog/app_lifespan.py
+++ films-catalog/app_lifespan.py
@@ -1,7 +1,6 @@
 import logging
 from contextlib import asynccontextmanager
 
-from api.v1.films.crud import film_storage
 from core import config
 from fastapi import FastAPI
 
@@ -9,5 +8,4 @@ from fastapi import FastAPI
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logging.basicConfig(level=config.LOG_LEVEL, format=config.LOG_FORMAT)
-    await film_storage.init_storage_from_state()
     yield  # Здесь приложение работает
