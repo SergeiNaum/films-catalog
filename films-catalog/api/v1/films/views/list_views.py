f07439d5b7749091f9a8edf20646d69000b3c2bf
Sergey <you@example.com>
2025-06-11 14:34:08 +0300

    add check for create_film with eq slugs

--- films-catalog/api/v1/films/views/list_views.py
+++ films-catalog/api/v1/films/views/list_views.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from schemas.film import FilmSchema, FilmSchemaCreate, FilmSchemaRead
 
 from api.v1.films.crud import film_storage
-from api.v1.films.dependencies.utils import api_token_or_user_basic_auth_required_for_unsafe_methods
+from api.v1.films.dependencies.utils import (
+    api_token_or_user_basic_auth_required_for_unsafe_methods,
+)
 
 router = APIRouter(
     prefix="/films",
@@ -39,4 +41,9 @@ async def get_films() -> list[FilmSchema]:
 async def create_film(
     film_create: FilmSchemaCreate,
 ) -> FilmSchema:
-    return await film_storage.create(film_create)
+    if not film_storage.get_by_slug(film_create.slug):
+        return await film_storage.create(film_create)
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT,
+        detail=f"film with this slug: {film_create.slug} already exist",
+    )
