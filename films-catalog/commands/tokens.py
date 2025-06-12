4ce30b57f3f75de8659b96692ea80d35498c7106
Sergey <you@example.com>
2025-06-12 14:17:03 +0300

    add cli command display_tokens

--- films-catalog/commands/tokens.py
+++ films-catalog/commands/tokens.py
@@ -12,10 +12,17 @@ app = typer.Typer(
 
 
 @app.command(help="Checks that the token exists")
-def check_token(token: Annotated[str, typer.Argument(help="Input token to check")]):
+def check_token(
+    token: Annotated[str, typer.Argument(help="Input token to check")],
+) -> None:
     _not = "[red]n[/red][yellow]o[/yellow][green]t[/green]"
     _token = f"[bright_white]{token}[/bright_white]"
     if redis_tokens.token_exist(token):
         print(f"token: {_token} [bright_green]exists[/bright_green]")
     else:
         print(f"token: {_token} {_not} [bright_red]exist ![/bright_red]")
+
+
+@app.command(help="Show all tokens in db")
+def display_tokens() -> None:
+    print(f"tokens: {redis_tokens.show_all_tokens()}")
