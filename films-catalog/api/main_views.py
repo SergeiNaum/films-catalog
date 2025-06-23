691f15d830789bc9d4ba38e5553e5d437735fa3c
Sergey <you@example.com>
2025-06-23 17:00:38 +0300

    refactor read_root endpoint

--- films-catalog/api/main_views.py
+++ films-catalog/api/main_views.py
@@ -5,8 +5,15 @@ router = APIRouter()
 
 
 @router.get("/")
-async def hello_world(request: Request) -> dict[str, str]:
-    docs_url = request.url.replace(path="/docs", query="")
+def read_root(
+    request: Request,
+    name: str = "World",
+) -> dict[str, str]:
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
     return {
+        "message": f"Hello {name}",
         "docs": str(docs_url),
     }
